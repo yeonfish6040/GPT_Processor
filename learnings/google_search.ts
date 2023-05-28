a60f75d5443773e827ca65bf87a1e9de7afc0df1
yeonfish <yeonfish6040@gmail.com>
2023-05-28 23:15:39 +0900

    update

--- learnings/google_search.ts
+++
@@ -1,9 +0,0 @@
-module.exports = (need: string, list: string) => {
-    return [
-        { role: "system", content: "Response as json string only." },
-        { role: "system", content: "You need "+need },
-        { role: "system", content: "Google search result is "+list },
-
-        { role: "user", content: "Give me the list of urls what you want to see" }
-    ]
-}
